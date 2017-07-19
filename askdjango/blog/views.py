f41c17c46722190a56aac731eb08dc5ad24c2e1b
Chinseok Lee <allieuslee@gmail.com>
2017-07-19 11:02:52 +0900

    post_list/post_detail 뷰 HttpResponse 간단 구현

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def post_list(request):
-    pass
+    return HttpResponse('글목록을 보여주겠소.')
 
 def post_detail(request, pk):
-    pass
+    return HttpResponse('{}번 글을 보여주겠소.'.format(pk))
 

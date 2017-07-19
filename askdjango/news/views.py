a69d6a006a4edd1f4ef26b02eb9bae0c640e0ea4
Chinseok Lee <allieuslee@gmail.com>
2017-07-19 11:20:35 +0900

    news.views.article_detail 뷰 구현

--- askdjango/news/views.py
+++ askdjango/news/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Article
 
-# Create your views here.
+def article_list(request):
+    return render(request, 'news/article_list.html', {
+        'article_list': Article.objects.all(),
+    })
+
+def article_detail(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'news/article_detail.html', {
+        'article': article,
+    })
+

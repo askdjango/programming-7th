d1582600a945c0e7255f5a1fe1cd8daa4cf99153
Chinseok Lee <allieuslee@gmail.com>
2017-07-19 11:23:32 +0900

    news article_list/article_detail 뷰에 한해 url reverse 적용

--- askdjango/news/urls.py
+++ askdjango/news/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.article_list),
-    url(r'^(?P<slug>[-\w]+)/$', views.article_detail),
+    url(r'^$', views.article_list, name='article_list'),
+    url(r'^(?P<slug>[-\w]+)/$', views.article_detail, name='article_detail'),
 ]
 

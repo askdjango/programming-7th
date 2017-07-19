0a7bfcd18f213c6a707317200d86d4375ca44df0
Chinseok Lee <allieuslee@gmail.com>
2017-07-19 11:10:19 +0900

    news.models.Article 모델 정의

--- askdjango/news/models.py
+++ askdjango/news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(allow_unicode=True)
+    content = models.TextField()
+

5261a6c2927faa0f3200985cc2ff9bbffc8c6421
Chinseok Lee <allieuslee@gmail.com>
2017-07-21 11:48:26 +0900

    news.models.Article 모델에 photo 이미지필드 추가

--- askdjango/news/models.py
+++ askdjango/news/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(allow_unicode=True)
+    photo = models.ImageField(blank=True)
     content = models.TextField()
     ip = models.GenericIPAddressField()
 

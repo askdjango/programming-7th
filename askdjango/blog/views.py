b762c63637ea9ba7686e25bf6ee72b8aba81f112
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 10:53:33 +0900

    빼먹은 post_list.html 템플릿 추가하고, 간단검색 구현

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,18 +1,25 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
 
 
-'''
 def post_list(request):
     qs = Post.objects.all()
+
+    query = request.GET.get('query', '')
+    if query:
+        condition = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = qs.filter(condition)
+
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
+        'query': query,
     })
-'''
 
-post_list = ListView.as_view(model=Post)
+
+# post_list = ListView.as_view(model=Post)
 
 
 

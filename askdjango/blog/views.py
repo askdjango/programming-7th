1001d0c6b679d53418d0a6245145f642e1d9f89d
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 10:38:01 +0900

    글 목록 구현

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return HttpResponse('글목록을 보여주겠소.')
+    qs = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
+
 
 def post_detail(request, pk):
     return HttpResponse('{}번 글을 보여주겠소.'.format(pk))

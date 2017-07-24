2f3cae3bfb1e56cc913514d6cf4ae75daa0b6f26
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 10:40:10 +0900

    blog.views.post_list 뷰를 CBV로 재구현

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
+'''
 def post_list(request):
     qs = Post.objects.all()
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
     })
+'''
+
+post_list = ListView.as_view(model=Post)
+
 
 
 def post_detail(request, pk):

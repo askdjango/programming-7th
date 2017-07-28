3372a46634cd701a64f667c6065581b4778445c0
Chinseok Lee <allieuslee@gmail.com>
2017-07-28 10:17:33 +0900

    blog.models.Post 모델에 user 외래키 추가

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.db import models
+# from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.CharField(max_length=20)
+    # author = models.CharField(max_length=20)
+
+    # user = models.ForeignKey(User)
+    # user = models.ForeignKey("auth.User")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

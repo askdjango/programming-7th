d78bbb99d5ebb2164c7aaeca2f020c5daa5c4d69
Chinseok Lee <allieuslee@gmail.com>
2017-07-28 10:39:15 +0900

    accounts 앱 생성 및 등록

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'bootstrap3',  # django-bootstrap3 설치 필요
     'debug_toolbar',
     'blog',

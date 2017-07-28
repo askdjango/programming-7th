832fba0015205d4e668b98fb519b0682284d5728
Chinseok Lee <allieuslee@gmail.com>
2017-07-28 10:32:47 +0900

    blog.views.post_new 뷰에서 로그인 유저 필드 지정

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -35,7 +35,9 @@ def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save()
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
             return redirect('/blog/{}/'.format(post.id))
     else:
         form = PostForm()
